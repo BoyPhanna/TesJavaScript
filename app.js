a44fa9a7061cce11c44d641fbe617cafe28e1a18
BoyPhanna <kimphanna56@gmail.com>
2024-04-23 23:56:16 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+// let x=26
+
+// x<26?console.log("x is equal 26"):console.log("x is not equal 26")
+
+
+// const result=(x>12)?44:67
+
+// console.log(result)
+
+// function paraFunc(x){
+//   console.log(x)
+// }
+
+// function Hello(){
+//   console.log("Hello function no parameter")
+// }
+
+// function ReturnFunc(){
+//   return "Hello boy"
+// }
+
+// function ReturnParaFunct(x){
+//   return "Hello "+x
+// }
+
+// console.log(ReturnParaFunct("Boy2"))
+
+
+const person = {fname:"John", lname:"Doe", age:25};
+
+
+for (let x in person) {
+  console.log(x +"  : "+person[x])
+}
+
+
+
